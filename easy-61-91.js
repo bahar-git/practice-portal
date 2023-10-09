e606a84537c1d428f82ec785668a79cbd5700ce3
bahar-git <bahar1nep@gmail.com>
2023-10-09 18:48:36 -0500

    solved 71-80

--- easy-61-91.js
+++ easy-61-91.js
@@ -279,7 +279,7 @@ Write a function called "getAllLetters".
 Given a word, "getAllLetters" returns an array containing every character in the word. Notes:* If given an empty string, it should return an empty array.
 var output = getAllLetters('Radagast');
 console.log(output); // --> ['R', 'a', 'd', 'a', 'g', 'a', 's', 't']
-Related Topics: Loop */
+Related Topics: Variables, Loop */
 
 // 1st way
 /* function getAllLetters(str) {
@@ -296,3 +296,214 @@ console.log(getAllLetters("Radagast")); */
   return output;
 }
 console.log(getAllLetters("Radagast")); */
+
+/* 71.  getAllWords
+Write a function called "getAllWords".
+Given a sentence, "getAllWords" returns an array containing every word in the sentence. Notes:* If given an empty string, it should return an empty array.
+var output = getAllWords('Radagast the Brown');
+console.log(output); // --> ['Radagast', 'the', 'Brown']
+Related Topics: Variables */
+
+/* function getAllWords(str) {
+  if (str.length === 0) return [];  // if(!str.length) return []
+  return str.split(" ");
+}
+console.log(getAllWords("Radagast the Brown")); */
+
+/* 72.  countdown
+Create a function that accepts a number as an input. Return a new array that counts down by one, from the number (as array's 'zeroth' element) down to 0 (as the last element). How long is this array?
+`countdown(5) // [5,4,3,2,1,0]`
+Related Topics: Loop */
+
+/* function countdown(num) {
+  let output = [];
+  for (let i = num; i >= 0; i--) {
+    output.push(i);
+  }
+  return output;
+}
+console.log(countdown(5)); */
+
+/* 73.  print
+Your function will receive an array with two numbers. Print the first value,and return the second.
+`print([3,5]) // print 3 and return 5`
+Related Topics: Variables */
+
+/* function print(arr) {
+  console.log(arr[0]);
+  return arr[1];
+}
+console.log(print([3, 5])); */
+
+/* 74.  isEitherEvenOrAreBoth7
+Write a function called "isEitherEvenOrAreBoth7".
+Given two numbers, 'isEitherEvenOrAreBoth7' returns true if at least one of the parameters is even, or, both of them are equal to 7.
+var output = isEitherEvenOrAreBoth7(3, 7);
+console.log(output); // --> false
+var output = isEitherEvenOrAreBoth7(2, 3);
+console.log(output); // --> true
+Related Topics: Variables */
+
+// 1st way
+/* function isEitherEvenOrAreBoth7(num1, num2) {
+  return num1 % 2 === 0 || num2 % 2 === 0
+    ? true
+    : num1 === 7 && num2 === 7
+    ? true
+    : false;
+}
+console.log(isEitherEvenOrAreBoth7(3, 7));
+console.log(isEitherEvenOrAreBoth7(2, 3));
+console.log(isEitherEvenOrAreBoth7(7, 7)); */
+
+// 2nd way
+/* function isEitherEvenOrAreBoth7(num1, num2) {
+  switch (true) {
+    case num1 % 2 === 0 || num2 % 2 === 0:
+      return true;
+    case num1 === 7 && num2 === 7:
+      return true;
+    default:
+      return false;
+  }
+}
+console.log(isEitherEvenOrAreBoth7(3, 7));
+console.log(isEitherEvenOrAreBoth7(2, 3));
+console.log(isEitherEvenOrAreBoth7(7, 7)); */
+
+/* 75.  isEitherEvenAndLessThan9
+Write a function called "isEitherEvenAndLessThan9".
+Given two numbers, 'isEitherEvenAndLessThan9' returns true if at least one of them is even, and, both of them are less than 9.
+var output = isEitherEvenAndLessThan9(2, 4);
+console.log(output); // --> true
+var output = isEitherEvenAndLessThan9(72, 2);
+console.log(output); // --> false
+Related Topics: Variables */
+
+// 1st way
+/* function isEitherEvenAndLessThan9(num1, num2) {
+  if ((num1 % 2 === 0 || num2 % 2 === 0) && num1 < 9 && num2 < 9) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isEitherEvenAndLessThan9(2, 4));
+console.log(isEitherEvenAndLessThan9(72, 2));
+console.log(isEitherEvenAndLessThan9(7, 1)); */
+
+// 2nd way
+/* function isEitherEvenAndLessThan9(num1, num2) {
+  switch (true) {
+    case (num1 % 2 === 0 || num2 % 2 === 0) && num1 < 9 && num2 < 9:
+      return true;
+    default:
+      return false;
+  }
+}
+console.log(isEitherEvenAndLessThan9(2, 4));
+console.log(isEitherEvenAndLessThan9(72, 2));
+console.log(isEitherEvenAndLessThan9(7, 1)); */
+
+/* 76.  fahrenheitToCelsius
+Kelvin wants to convert between temperature scales.
+Create fahrenheitToCelsius(fDegrees) that accepts a number of degrees in Fahrenheit, and returns the equivalent temperature as expressed in Celsius degrees.
+Notes:* Fahrenheit = (9/5 * Celsius) + 32.
+`fahrenheitToCelsius(90) // 32`
+Related Topics: Variables */
+
+/* function fahrenheitToCelsius(fDegrees) {
+  return Math.floor(((fDegrees - 32) * 5) / 9);
+}
+console.log(fahrenheitToCelsius(90)); */
+
+/* 77.  celsiusToFahrenheit
+Create celsiusToFahrenheit(cDegrees) that accepts number of degrees Celsius, and returns the equivalent temperature expressed in Fahrenheit degrees.
+`celsiusToFahrenheit(27) // 80`
+Related Topics: Variables */
+
+/* function celsiusToFahrenheit(cDegrees) {
+  return Math.floor((9 / 5) * cDegrees + 32);
+}
+console.log(celsiusToFahrenheit(27)); */
+
+/* 78.  makeitBig
+Write a function called "makeitBig".
+Given an array, write a function that changes all positive numbers in the array to "big".
+`makeitBig([-1,3,5,-5]) // [-1, "big","big",-5];`
+Related Topics: Loop */
+
+// 1st way - same array
+/* function makeitBig(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    arr[i] > 0 ? arr.splice(i, 1, "big") : null;
+  }
+  return arr;
+}
+console.log(makeitBig([-1, 3, 5, -5])); */
+
+// 2nd way - same array
+/* function makeitBig(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      arr[i] = "big";
+    } else {
+      arr[i] = arr[i];
+    }
+  }
+  return arr;
+}
+console.log(makeitBig([-1, 3, 5, -5])); */
+
+// 3rd way - new array
+/* function makeitBig(arr) {
+  let result = [];
+  for (let el of arr) {
+    el > 0 ? result.push("big") : result.push(el);
+  }
+  return result;
+}
+console.log(makeitBig([-1, 3, 5, -5])); */
+
+/* 79.  double
+Write a function called "double".
+Given array, create a function to return a new array where each value in the original has been doubled. Calling double([1,2,3]) should return [2, 4, 6] without changing original.
+`double([1,2,3]) // [2, 4, 6]`
+Related Topics: Array, Loop */
+
+/* function double(arr) {
+  let result = [];
+  for (let el of arr) {
+    result.push(el + el);
+  }
+  return result;
+}
+console.log(double([1, 2, 3])); */
+
+/* 80.  prevLen
+You are passed an array containing string, Working within that same array, replace each string with a number - the length of the string previous array index - and return the array 'k
+`prevLen(['av','s','asdb','adfgb']) // [5,2,1,4]`
+Related Topics: Array, Loop */
+
+// 1st way - same array
+/* function prevLen(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr.splice(i, 1, arr[i].length);
+  }
+  const lastOne = arr.pop();
+  arr.unshift(lastOne);
+  return arr;
+}
+console.log(prevLen(["av", "s", "asdb", "adfgb"])); */
+
+// 2nd way - new array
+/* function prevLen(arr) {
+  const solution = [];
+  for (let i = 0; i < arr.length; i++) {
+    solution.push(arr[i].length);
+  }
+  const lastElement = solution.pop();
+  solution.unshift(lastElement);
+  return solution;
+}
+console.log(prevLen(["av", "s", "asdb", "adfgb"])); */
