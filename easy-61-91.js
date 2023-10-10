6d876a8501beeacec9e09afb219c03e608e496f1
bahar-git <bahar1nep@gmail.com>
2023-10-09 22:17:08 -0500

    solved 81-91

--- easy-61-91.js
+++ easy-61-91.js
@@ -507,3 +507,226 @@ console.log(prevLen(["av", "s", "asdb", "adfgb"])); */
   return solution;
 }
 console.log(prevLen(["av", "s", "asdb", "adfgb"])); */
+
+/* 81.  addSeven
+Build function that accepts array. Return an array with all values except first, adding 7 to each. Do not alter the original array.
+`addSeven([1,2,5,10]) // [9,12,17]`
+Related Topics: Array, Loop */
+
+// 1st way - same array
+/* function addSeven(arr) {
+  let i = 0;
+  while (i <= arr.length - 1) {
+    arr.splice(i, 1, arr[i] + 7);
+    i++;
+  }
+  arr.shift();
+  return arr;
+}
+console.log(addSeven([1, 2, 5, 10])); */
+
+// 2nd way - new array
+/* function addSeven(arr) {
+  arr.shift();
+  let output = [];
+  for (let el of arr) {
+    output.push(el + 7);
+  }
+  return output;
+}
+console.log(addSeven([1, 2, 5, 10])); */
+
+/* 82.  isPersonOldEnoughToDrinkAndDrive
+Write a function called "isPersonOldEnoughToDrinkAndDrive".
+Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrinkAndDrive" returns whether the given person is old enough to legally drink and drive in the United States.
+Notes:* The legal drinking age in the United States is 21.* The legal driving age in the United States is 16.* It is always illegal to drink and drive in the United States.
+var obj = {
+  age: 45
+};
+var output = isPersonOldEnoughToDrinkAndDrive(obj);
+console.log(output); // --> false
+Related Topics: Object */
+
+/* var obj = {
+  age: 45,
+};
+
+function isPersonOldEnoughToDrinkAndDrive(person) {
+  return person.age >= 16 ? false : false;
+}
+console.log(isPersonOldEnoughToDrinkAndDrive(obj)); */
+
+/* 83.  isPersonOldEnoughToDrive
+Write a function called "isPersonOldEnoughToDrive".
+Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrive" returns whether the given person is old enough to drive.
+Notes:* The legal driving age in the United States is 16.
+var obj = {
+  age: 16
+};
+var output = isPersonOldEnoughToDrive(obj);
+console.log(output); // --> true
+Related Topics: Object */
+
+/* var obj = {
+  age: 16,
+};
+
+function isPersonOldEnoughToDrive(person) {
+  return person.age >= 16 ? true : false;
+}
+console.log(isPersonOldEnoughToDrive(obj)); */
+
+/* 84.  isPersonOldEnoughToVote
+Write a function called "isPersonOldEnoughToVote".
+Given a "person" object, that contains an "age" property, "isPersonOldEnoughToVote" returns whether the given person is old enough to vote.
+Notes:* The legal voting age in the United States is 18.
+var obj = {
+  age: 19
+};
+var output = isPersonOldEnoughToVote(obj);
+console.log(output); // --> true
+Related Topics: Object */
+
+/* var obj = {
+  age: 19,
+};
+
+function isPersonOldEnoughToVote(person) {
+  return person.age >= 18 ? true : false;
+}
+console.log(isPersonOldEnoughToVote(obj)); */
+
+/* 85.  isPersonOldEnoughToDrink
+Write a function called "isPersonOldEnoughToDrink".
+Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrink" returns whether the given person is old enough to drink.
+Notes:* The legal drinking age in the United States is 21.
+var obj = {
+  age: 16
+};
+var output = isPersonOldEnoughToDrink(obj);
+console.log(output); // --> false
+Related Topics: Object */
+
+/* var obj = {
+  age: 16,
+};
+
+function isPersonOldEnoughToDrink(person) {
+  return person.age >= 21 ? true : false;
+}
+console.log(isPersonOldEnoughToDrink(obj)); */
+
+/* 86.  getProperty
+Write a function called "getProperty".
+Given an object and a key, "getProperty" returns the value of the property at the given key. Notes:* If there is no property at the given key, it should return undefined.
+var obj = {
+  key: 'value'
+};
+var output = getProperty(obj, 'key');
+console.log(output); // --> 'value'
+Related Topics: Object */
+
+/* var obj = {
+  key: "value",
+};
+
+// 1st way
+function getProperty(obj, key) {
+  return obj[key] ? obj[key] : undefined;
+}
+console.log(getProperty(obj, "key")); */
+
+// 2nd way
+/* function getProperty(obj, key) {
+  return obj.hasOwnProperty(key) ? obj[key] : undefined;
+}
+console.log(getProperty(obj, "key")); */
+
+/* 87.  addProperty
+Write a function called "addProperty".
+Given an object, and a key, "addProperty" sets a new property on the given object with a value of true.
+var myObj = {};
+addProperty(myObj, 'myProperty');
+console.log(myObj.myProperty); // --> true
+Related Topics: Object */
+
+/* var myObj = {};
+function addProperty(obj, key) {
+  obj[key] = true;
+  return obj;
+}
+console.log(addProperty(myObj, "myProperty")); */
+
+/* 88.  removeProperty
+Write a function called "removeProperty".
+Given an object and a key, "removeProperty" removes the given key from the given object.
+var obj = {
+  name: 'Sam',
+  age: 20
+}
+removeProperty(obj, 'name');
+console.log([obj.name](http://obj.name/)); // --> undefined
+Related Topics: Object */
+
+/* var obj = {
+  name: "Sam",
+  age: 20,
+};
+
+function removeProperty(obj, key) {
+  delete obj[key];
+  return obj;
+}
+console.log(removeProperty(obj, "name")); */
+
+/* 89.  getLengthOfThreeWords
+Write a function called "getLengthOfThreeWords".
+Given 3 words, "getLengthOfThreeWords" returns the sum of their lengths.
+var output = getLengthOfThreeWords('some', 'other', 'words');
+console.log(output); // --> 14
+Related Topics: Variables */
+
+/* function getLengthOfThreeWords(str1, str2, str3) {
+  return str1.length + str2.length + str3.length;
+}
+console.log(getLengthOfThreeWords("some", "other", "words")); */
+
+/* 90.  addFullNameProperty
+Write a function called "addFullNameProperty".
+Given an object that has a "firstName" property and a "lastName" property, "addFullNameProperty" returns a "fullName" property whose value is a string with the first name and last name separated by a space.
+var person = {
+  firstName: 'Jade',
+  lastName: 'Smith'
+};
+addFullNameProperty(person);
+console.log(person.fullName); // --> 'Jade Smith'
+Related Topics: Object */
+
+/* var person = {
+  firstName: "Jade",
+  lastName: "Smith",
+};
+
+function addFullNameProperty(obj) {
+  obj.fullName = `${obj.firstName} ${obj.lastName}`;
+  return obj;
+}
+console.log(addFullNameProperty(person)); */
+
+/* 91.  addArrayProperty
+Write a function called "addArrayProperty".
+Given an object, a key, and an array, "addArrayProperty" sets a new property on the object at the given key, with its value set to the given array.
+return myObj data.
+var myObj = {};
+var myArray = [1, 3];
+addArrayProperty(myObj, 'myProperty', myArray);
+console.log(myObj.myProperty); // --> [1, 3]
+Related Topics: Object */
+
+/* var myObj = {};
+var myArray = [1, 3];
+function addArrayProperty(obj, key, arr) {
+  obj[key] = arr;
+  return obj;
+}
+console.log(addArrayProperty(myObj, "myProperty", myArray)); */
