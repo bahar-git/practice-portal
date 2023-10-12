c84a42c7f80471b92ea416fb2e1504333322befa
bahar-git <bahar1nep@gmail.com>
2023-10-12 18:58:23 -0500

    solved 52-60

--- junior-31-67.js
+++ junior-31-67.js
@@ -552,3 +552,277 @@ Related Topics: Function, Loop */
   return sum;
 }
 console.log(factorial(5)); */
+
+////////////////////////////////////////////////////////////////
+
+/* 51.  generateCoinChange
+Create a function which accepts cents, compute and print how to represent that amount with smallest number of coins. pennies (1 cent), nickels (5 cents), dimes (10 cents), and quarters (25 cents), half-dollar(50 cents) and dollar (100 cents).
+`generateCoinChange(194)
+194 cents can be represented by:
+[
+    dollars:    1
+    quarters:    3
+    dimes:      1
+    nickels:    1
+    pennies:    4
+]`
+`generateCoinChange(23)`
+`out: [0,0,2,0,3]`
+Related Topics: Function, Array */
+
+/* function generateCoinChange(num) {
+  const coinChange = {
+    dollars: 100,
+    quarters: 25,
+    dimes: 10,
+    nickels: 5,
+    pennies: 1,
+  };
+  let output=[]
+
+}
+console.log(generateCoinChange(194)); */
+
+// NEED TO SOLVE
+
+////////////////////////////////////////////////////////////////
+
+/* 52.  sumDigits
+Write a function called "sumDigits".
+Given a number, "sumDigits" returns the sum of all its digits.
+var output = sumDigits(1148);
+console.log(output); // --> 14
+If the number is negative, the first digit should count as negative.
+var output = sumDigits(-316);
+console.log(output); // --> 4
+Notes:  In order to use some of the methods that will be most helpful to you, you will most likely want to do some string to number conversion and vice versa.
+Be sure to familiarize yourself with the "toString" method, as well as the "Number" function.
+Related Topics: Function */
+
+// 1st way
+/* function sumDigits(num) {
+  let numStr = String(num);
+  let sum = 0;
+  if (num >= 0) {
+    for (let i = 0; i < numStr.length; i++) {
+      sum += Number(numStr[i]);
+    }
+  } else {
+    let splitNumStr = numStr.split("");
+    splitNumStr.splice(0, 1);
+    splitNumStr[0] *= -1;
+    for (let i = 0; i < splitNumStr.length; i++) {
+      sum += Number(splitNumStr[i]);
+    }
+  }
+  return sum;
+}
+console.log(sumDigits(1148));
+console.log(sumDigits(-316)); */
+
+// 2nd way
+/* function sumDigits(num) {
+  let isNeg = num < 0;
+  let numbers = (isNeg ? String(num).slice(1) : String(num))
+    .split("")
+    .map(Number);   // .map(el=>Number(el))
+  if (isNeg) numbers[0] *= -1;
+  return numbers.reduce((acc, currVal) => {
+    return acc + currVal;
+  });
+}
+console.log(sumDigits(1148));
+console.log(sumDigits(-316)); */
+
+////////////////////////////////////////////////////////////////
+
+/* 53.  repeatString
+Write a function called "repeatString".
+Given a string and a number, "repeatString" returns the given string repeated the given number of times.
+var output = repeatString('code', 3);
+console.log(output); // --> 'codecodecode'
+Related Topics: Function, String */
+
+/* function repeatString(str, num) {
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+  return result;
+}
+console.log(repeatString("code", 3)); */
+
+////////////////////////////////////////////////////////////////
+
+/* 54.  getLongestOfThreeWords
+Write a function called "getLongestOfThreeWords".
+Given 3 words, "getLongestOfThreeWords" returns the longest of three words.
+Notes:  If there is a tie, it should return the first word in the tie.
+var output = getLongestOfThreeWords('these', 'three', 'words');
+console.log(output); // --> 'these'
+Related Topics: Function, String */
+
+// 1st way
+/* function getLongestOfThreeWords(str1, str2, str3) {
+  let strArr = [str1, str2, str3]; // strArr.push(str1, str2, str3)
+  let output = strArr[0];
+  for (let el of strArr) {
+    if (output.length < el.length) {
+      output = el;
+    }
+  }
+  return output;
+}
+console.log(getLongestOfThreeWords("these", "three", "words")); */
+
+// 2nd way
+/* function getLongestOfThreeWords(str1, str2, str3) {
+  let output = "";
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i].length > output.length) {
+      output = arguments[i];
+    }
+  }
+  return output;
+}
+console.log(getLongestOfThreeWords("these", "three", "words")); */
+
+////////////////////////////////////////////////////////////////
+
+/* 55.  findShortestOfThreeWords
+Write a function called "findShortestOfThreeWords".
+Given 3 strings, "findShortestOfThreeWords" returns the shortest of the given strings.
+Notes:  If there are ties, it should return the first word in the parameters list.
+var output = findShortestOfThreeWords('a', 'two', 'three');
+console.log(output); // --> 'a'
+Related Topics: Function, String */
+
+// 1st way
+/* function findShortestOfThreeWords(str1, str2, str3) {
+  let strArr = [str1, str2, str3]; // strArr.push(str1, str2, str3)
+  let result = strArr[0];
+  for (let el of strArr) {
+    if (el.length < result.length) {
+      result = el;
+    }
+  }
+  return result;
+}
+console.log(findShortestOfThreeWords("a", "two", "three")); */
+
+// 2nd way
+/* function findShortestOfThreeWords(str1, str2, str3) {
+  let result = arguments[0];
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i].length < result.length) {
+      result = arguments[i];
+    }
+  }
+  return result;
+}
+console.log(findShortestOfThreeWords("a", "two", "three")); */
+
+////////////////////////////////////////////////////////////////
+
+/* 56.  isOddWithoutModulo
+Write a function called "isOddWithoutModulo".
+Given a number, "isOddWithoutModulo" returns whether the passed in number is odd.
+Note:  It does so without using the modulo operator (%).
+It should work for negative numbers and zero.
+var output = isOddWithoutModulo(17);
+console.log(output); // --> true
+Related Topics: Function */
+
+/* function isOddWithoutModulo(num) {
+  let result = false;
+  let divided = Math.floor(num / 2);
+  let remainder = num - divided * 2;
+  if (remainder !== 0) result = true;
+  return result;
+}
+console.log(isOddWithoutModulo(17)); */
+
+////////////////////////////////////////////////////////////////
+
+/* 57.  isEvenWithoutModulo
+Write a function called "isEvenWithoutModulo".
+Given a number, "isEvenWithoutModulo" returns whether it is even.
+Notes:  It does so without using the modulo operator (%).
+It should work for negative numbers and zero.
+var output = isEvenWithoutModulo(8);
+console.log(output); // --> true
+Related Topics: Function */
+
+/* function isEvenWithoutModulo(num) {
+  let result = false;
+  let divided = Math.floor(num / 2);
+  let remainder = num - divided * 2;
+  if (remainder === 0) result = true;
+  return result;
+}
+console.log(isEvenWithoutModulo(8)); */
+
+////////////////////////////////////////////////////////////////
+
+/* 58.  multiplyBetween
+Write a function called "multiplyBetween".
+Given 2 integers, "multiplyBetween" returns the product between the two given integers, beginning at num1, and excluding num2.
+Notes:  The product between 1 and 4 is 1 * 2 * 3 = 6.
+If num2 is not greater than num1, it should return 0.
+var output = multiplyBetween(2, 5);
+console.log(output); // --> 24
+Related Topics: Function */
+
+/* function multiplyBetween(num1, num2) {
+  if (num2 < num1) return 0;
+  let result = 1;
+  for (let i = num1; i < num2; i++) {
+    result *= i;
+  }
+  return result;
+}
+console.log(multiplyBetween(2, 5)); */
+
+////////////////////////////////////////////////////////////////
+
+/* 59.  transformFirstLast
+Write a function called "transformFirstLast".
+Transform first and last item to an object.
+var arr = [1,2,3,4]
+// {first: 1, last: 4}
+Related Topics: Function, Object, Array */
+
+/* function transformFirstLast(arr) {
+  let obj = {};
+  obj.first = arr[0];
+  obj.last = arr[arr.length - 1];
+  return obj;
+}
+console.log(transformFirstLast([1, 2, 3, 4])); */
+
+////////////////////////////////////////////////////////////////
+
+/* 60.  getAllKeys
+Write a function called "getAllKeys".
+Return all keys from a given array.
+var obj = {a: 1, b: 4}
+getAllKeys(obj) // ['a', 'b']
+Related Topics: Function, Object, Array */
+
+/* var obj = { a: 1, b: 4 };
+
+// 1st way
+function getAllKeys(obj) {
+  let arr = [];
+  for (let key in obj) {
+    arr.push(key);
+  }
+  return arr;
+}
+console.log(getAllKeys(obj)); */
+
+// 2nd way
+/* function getAllKeys(obj) {
+  return Object.keys(obj);
+}
+console.log(getAllKeys(obj)); */
