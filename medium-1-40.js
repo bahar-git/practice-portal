590f18ad3a99d60d19e6d7ceaafe23b704e0fc13
bahar-git <bahar1nep@gmail.com>
2023-10-14 14:10:58 -0500

    solved 11-15

--- medium-1-40.js
+++ medium-1-40.js
@@ -317,3 +317,174 @@ console.log(count("Honey pie, you are driving me crazy")); */
   return result.length;
 };
 console.log(count("Honey pie, you are driving me crazy")); */
+
+////////////////////////////////////////////////////////////////
+
+/* 11.  remove
+Remove Shorter Strings
+Given an array of strings and length len, find and remove any strings that are shorter than given length len from the array.
+remove(["geeks", "for", "geeks"], 3) // ["geeks", "geeks"]
+remove(["hello", "react", "javascript", "ruby"], 6) // ["javascript"]
+Related Topics: Function */
+
+// 1st way - same array
+/* const remove = (arr, len) => {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i].length <= len) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
+};
+console.log(remove(["geeks", "for", "geeks"], 3));
+console.log(remove(["hello", "react", "javascript", "ruby"], 6)); */
+
+// 2nd way - same array
+/* const remove = (arr, len) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length <= len) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
+};
+console.log(remove(["geeks", "for", "geeks"], 3));
+console.log(remove(["hello", "react", "javascript", "ruby"], 6)); */
+
+// 3rd way - new array
+/* const remove = (arr, len) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > len) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+console.log(remove(["geeks", "for", "geeks"], 3));
+console.log(remove(["hello", "react", "javascript", "ruby"], 6)); */
+
+// 4th way
+/* const remove = (arr, len) => {
+  return arr.filter((el) => el.length > len);
+};
+console.log(remove(["geeks", "for", "geeks"], 3));
+console.log(remove(["hello", "react", "javascript", "ruby"], 6)); */
+
+////////////////////////////////////////////////////////////////
+
+/* 12.  range
+Write a JavaScript program to get the integers in range
+`range(2, 9) // [3, 4, 5, 6, 7, 8]`
+Related Topics: Function */
+
+/* const range = (num1, num2) => {
+  let output = [];
+  if (num1 >= 0 && num2 >= 0) {
+    let num = num1 + 1;
+    while (num < num2) {
+      output.push(num);
+      num++;
+    }
+  } else if (num1 < 0 && num2 < 0) {
+    let num = num1 - 1;
+    while (num > num2) {
+      output.push(num);
+      num--;
+    }
+  }
+  return output;
+};
+console.log(range(2, 9));
+console.log(range(-10, -15)); */
+
+////////////////////////////////////////////////////////////////
+
+/* 13.  myChoice
+We have the following array:
+const color = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"].
+Write a JavaScript program to display the colors in the following way :
+`"1st choice is Blue."
+"2nd choice is Green."
+"3rd choice is Red."
+...`
+Related Topics: Function */
+
+/* const myChoice = (arr) => {
+  let output = "";
+  for (let i = 0; i < arr.length; i++) {
+    switch (true) {
+      case i + 1 === 1:
+        output += `${i + 1}st choice is ${arr[i]}. `;
+        break;
+      case i + 1 === 2:
+        output += `${i + 1}nd choice is ${arr[i]}. `;
+        break;
+      case i + 1 === 3:
+        output += `${i + 1}rd choice is ${arr[i]}. `;
+        break;
+      default:
+        output += `${i + 1}th choice is ${arr[i]}. `;
+    }
+  }
+  return output.trim();
+};
+console.log(
+  myChoice(["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"])
+); */
+
+////////////////////////////////////////////////////////////////
+
+/* 14.  search_word
+Write a JavaScript function to find a word within a string.
+`search_word('The quick brown fox', 'fox') // => "'fox' was found 1 times."
+search_word('aa, bb, cc, dd, aa', 'aa'); // -> "'aa' was found 2 times."`
+Related Topics: Function */
+
+/* const search_word = (str, word) => {
+  let counter = 0;
+  let arrStr = str.split(/[" ",]+/);
+  for (let i = 0; i < arrStr.length; i++) {
+    if (arrStr[i] === word) {
+      counter++;
+    }
+  }
+  return `'${word}' was found ${counter} times`;
+};
+console.log(search_word("The quick brown fox", "fox"));
+console.log(search_word("aa, bb, cc, dd, aa", "aa")); */
+
+////////////////////////////////////////////////////////////////
+
+/* 15.  parameterize
+Write a JavaScript function to parameterize a string.
+`parameterize('Fluffy Whisker from Seattle') // => fluffy-whisker-from-seattle`
+Related Topics: Function */
+
+// 1st way
+/* const parameterize = (str) => {
+  let output = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      output += "-";
+    } else {
+      output += str[i].toLowerCase();
+    }
+  }
+  return output;
+};
+console.log(parameterize("Fluffy Whisker from Seattle")); */
+
+// 2nd way
+/* const parameterize = (str) => {
+  return str.replaceAll(" ", "-").toLowerCase();
+};
+console.log(parameterize("Fluffy Whisker from Seattle")); */
+
+// 3rd way
+/* const parameterize = (str) => {
+  let result = str.toLowerCase().split(" ").join("-");
+  return result;
+};
+console.log(parameterize("Fluffy Whisker from Seattle")); */
